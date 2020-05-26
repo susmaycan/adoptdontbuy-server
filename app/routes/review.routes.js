1000cdd56a8126ea217d84fb63a25ed2842a9e0d
asdy <adoptdontbuy2019@gmail.com>
2020-05-26 13:36:04 +0200

    Fix reviews

--- app/routes/review.routes.js
+++ app/routes/review.routes.js
@@ -1,18 +1,13 @@
 module.exports = (app) => {
     const reviews = require('../controllers/review.controller');
-    
-    // Create a new review
+
     app.post('/review', reviews.create);
 
-    // Retrieve all reviews
     app.get('/review', reviews.findAll);
 
-    // Retrieve one review
     app.get('/review/:reviewId', reviews.findOne);
 
-     // Update a Note with noteId
      app.put('/review/:reviewId', reviews.update);
 
-     // Delete a Note with noteId
      app.delete('/review/:reviewId', reviews.delete);
 }
